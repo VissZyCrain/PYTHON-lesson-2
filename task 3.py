1dba6e8d8b232ab9ec18ed1d610e3a6384de25c8
VissZyCrain <aletuchin@yandex.ru>
2022-11-30 17:20:56 +0300

    realized task 3

--- task 3.py
+++ task 3.py
@@ -5,23 +5,35 @@
 # Ввод: 4
 # [-4, -3, -2, -1, 0, 1, 2, 3,4]
 
-
 import os
 os.system('cls')
 
 print('Задача № 3')
 
-from random import randint
-
-def list(n):
-    list = []
-    for i in range(n):
-        list.append(randint(-n, n))
-    return list
-
 n = int(input('Введите число N: '))
-numbers = list(n)
-print(numbers)
-b = open('file.txt','r')
-result = numbers[int(b.readline())] * numbers[int(b.readline(2))]
-print(result)
+list1 = [i for i in range(-n, n+1)]
+print(list1)
+list2 = [2, 2, 3, 1, 8]
+proiz = 1
+for index in list2:
+    proiz *= (list1[index])
+    print(list1[index])
+print(proiz)
+
+
+# Решение через .txt
+
+# from random import randint
+
+# def list(n):
+#     list = []
+#     for i in range(2*n+1):
+#         list.append(randint(-n, n))
+#     return list
+
+# n = int(input('Введите число N: '))
+# numbers = list(n)
+# print(numbers)
+# # x = open('file.txt','r')
+# # result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+# # print(result)
